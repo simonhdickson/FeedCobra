aeb8ffe722f0794be582a81c345aeba70a8e70f5
Simon Dickson <simon.dickson@pebblecode.com>
2013-03-22 13:58:53 +0000

    Intial commit

---
+++ SiFeed/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Argotic.Syndication;
+using SiFeed.Services;
+
+namespace SiFeed.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int skip = 0, int count = 10)
+        {
+            var service = new FeedService();
+
+            var feed = service.GetFeedForUser(User.Identity.Name).Skip(skip).Take(count);
+
+            return View(feed);
+        }
+
+    }
+}
---
+++ SiFeed/Scripts/Services/FeedService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Argotic.Syndication;
+
+namespace SiFeed.Services
+{
+    public class FeedService
+    {
+        public IEnumerable<RssItem> GetFeedForUser(string username)
+        {
+            var feed = RssFeed.Create(new Uri("http://www.pwop.com/feed.aspx?show=dotnetrocks&filetype=master"));
+
+            return feed.Channel.Items;
+        }
+    }
+}
