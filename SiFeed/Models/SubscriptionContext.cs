726be339aa6bdc35605f5de8e6bb0328b78cfe99
Simon Dickson <simon.dickson@pebblecode.com>
2013-03-22 18:28:09 +0000

    Adds prelim work on auth, and starts to add feeds

---
+++ SiFeed/Models/SubscriptionContext.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace SiFeed.Models
+{
+    public class SubscriptionContext : DbContext
+    {
+        public SubscriptionContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+
+    [Table("Subscription")]
+    public class Subscription
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Feed { get; set; }
+    }
+
+    public class RegisterSubscriptionModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Feed { get; set; }
+    }
+}
--- SiFeed/Scripts/Services/FeedService.cs
+++ SiFeed/Scripts/Services/FeedService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Argotic.Syndication;
+using SiFeed.Models;
 
 namespace SiFeed.Services
 {
@@ -11,9 +12,11 @@ namespace SiFeed.Services
     {
         public IEnumerable<RssItem> GetFeedForUser(string username)
         {
-            var feed = RssFeed.Create(new Uri("http://www.pwop.com/feed.aspx?show=dotnetrocks&filetype=master"));
-
-            return feed.Channel.Items;
+            using (var db = new UsersContext())
+            {
+                UserProfile user = db.UserProfiles.FirstOrDefault(u => u.UserName.ToLower() == username.ToLower());
+                return feed.Channel.Items;
+            }
         }
     }
 }
