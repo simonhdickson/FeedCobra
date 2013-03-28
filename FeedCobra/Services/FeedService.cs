8d272bc26ddef5dacba84794fd2d304e9a218472
Simon Dickson <simon.dickson@pebblecode.com>
2013-03-28 17:21:49 +0000

    Move Feed Service

---
+++ FeedCobra/Services/FeedService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Argotic.Syndication;
+using FeedCobra.Models;
+
+namespace FeedCobra.Services
+{
+    public class FeedService : IFeedService<RssItem>
+    {
+        private readonly SubscriptionContext subscriptionContext;
+
+        public FeedService(SubscriptionContext subscriptionContext)
+        {
+            this.subscriptionContext = subscriptionContext;
+        }
+
+        public IEnumerable<RssItem> GetFeedForUser(string username)
+        {
+            var feed = subscriptionContext.Subscriptions
+                            .Where(s => s.Feed.Type == "rss")
+                            .Select(s => s.Feed.XmlUrl).ToArray()
+                            .Select(f => RssFeed.Create(new Uri(f)))
+                            .SelectMany(r => r.Channel.Items)
+                            .OrderBy(i => i.PublicationDate);
+            return feed;
+        }
+    }
+}
---
+++ FeedCobra/Services/IFeedService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FeedCobra.Services
+{
+    public interface IFeedService<out TItem>
+    {
+        IEnumerable<TItem> GetFeedForUser(string username);
+    }
+}
